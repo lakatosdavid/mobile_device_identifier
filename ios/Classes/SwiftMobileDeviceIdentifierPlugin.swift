983b243162b644277e2d61b98d5cbe7333939d41
lakatos.david <Nokia511>
2026-06-10 13:22:59 +0200

    swift package manager

--- ios/Classes/SwiftMobileDeviceIdentifierPlugin.swift
+++ ios/Classes/SwiftMobileDeviceIdentifierPlugin.swift
@@ -1,42 +1,58 @@
 import Flutter
 import UIKit
-import JNKeychain
+import Security
 
 public class SwiftMobileDeviceIdentifierPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-      let channel = FlutterMethodChannel(name: "mobile_device_identifier", binaryMessenger: registrar.messenger())
-      let instance = SwiftMobileDeviceIdentifierPlugin()
-      registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "getDeviceId":
-        let key = "com.alfanthariq.mobile_device_identifier/device_id"
-        result(self.getDeviceIdentifierFromKeychain(key: key))
-    default:
-        result(FlutterMethodNotImplemented)
-        fatalError()
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "mobile_device_identifier", binaryMessenger: registrar.messenger())
+        let instance = SwiftMobileDeviceIdentifierPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
     }
-  }
 
-  func getDeviceIdentifierFromKeychain(key : String) -> String {
-    // try to get value from keychain
-    var deviceUDID = self.keychain_valueForKey(key) as? String
-    if deviceUDID == nil {
-        deviceUDID = UIDevice.current.identifierForVendor!.uuidString
-        // save new value in keychain
-        self.keychain_setObject(deviceUDID! as AnyObject, forKey: key)
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+        case "getDeviceId":
+            let key = "com.alfanthariq.mobile_device_identifier/device_id"
+            result(self.getDeviceIdentifierFromKeychain(key: key))
+        default:
+            result(FlutterMethodNotImplemented)
+        }
     }
-    return deviceUDID!
-  }
 
-  func keychain_setObject(_ object: AnyObject, forKey: String) {
-    JNKeychain.saveValue(object, forKey: forKey)
-  }
+    func getDeviceIdentifierFromKeychain(key: String) -> String {
+        var deviceUDID = keychainLoad(key: key)
+        if deviceUDID == nil {
+            deviceUDID = UIDevice.current.identifierForVendor?.uuidString ?? UUID().uuidString
+            keychainSave(key: key, value: deviceUDID!)
+        }
+        return deviceUDID!
+    }
 
-  func keychain_valueForKey(_ key: String) -> AnyObject? {
-    let value = JNKeychain.loadValue(forKey: key)
-    return value as AnyObject?
-  }
+    private func keychainSave(key: String, value: String) {
+        let data = value.data(using: .utf8)!
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: key,
+            kSecValueData as String: data,
+        ]
+        SecItemDelete(query as CFDictionary)
+        SecItemAdd(query as CFDictionary, nil)
+    }
+
+    private func keychainLoad(key: String) -> String? {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: key,
+            kSecReturnData as String: true,
+            kSecMatchLimit as String: kSecMatchLimitOne,
+        ]
+        var item: CFTypeRef?
+        let status = SecItemCopyMatching(query as CFDictionary, &item)
+        guard status == errSecSuccess,
+              let data = item as? Data,
+              let value = String(data: data, encoding: .utf8) else {
+            return nil
+        }
+        return value
+    }
 }
